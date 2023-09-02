02229860ad7e41c0f8df2f33efb39ac79dd93b24
KingNightmare97 <manuel.brinocoli.10@gmail.com>
2023-09-02 20:38:45 -0300

    Correcciones de errores de WorkRepository

--- src/main/java/com/serviceYa/JobsHub/Repositories/WorkRepository.java
+++ src/main/java/com/serviceYa/JobsHub/Repositories/WorkRepository.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.serviceYa.JobsHub.Repositories;
 
-/**
- *
- * @author Manuel Briñocoli
- */
-public interface WorkRepository {
-    
+import com.serviceYa.JobsHub.entities.User;
+import com.serviceYa.JobsHub.entities.Work;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+
+public interface WorkRepository extends JpaRepository<Work, String>{
+        @Query("SELECT w FROM Work w WHERE w.userProviderld = :provider_id")
+        List<Work> getWorkByUserProvider(@Param("provider_id") User provider);
+        
+        @Query("SELECT w FROM Work w WHERE w.userCustomerld = :customer_id")
+        List<Work> getWorkByUserCustomer(@Param("customer_id") User customer);
 }
