6930225d6f021a70edb8db7ce192db52c8cc6044
KingNightmare97 <manuel.brinocoli.10@gmail.com>
2023-09-02 17:51:46 -0300

    Exception modificado

--- src/main/java/com/serviceYa/JobsHub/Exceptions/MiException.java
+++ src/main/java/com/serviceYa/JobsHub/Exceptions/MiException.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.serviceYa.JobsHub.Exceptions;
 
-/**
- *
- * @author Manuel Briñocoli
- */
-public class MiException {
-    
+public class MiException extends Exception{
+    public MiException (String msg) {
+        super (msg);
+    }
 }
